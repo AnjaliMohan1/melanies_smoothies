db3f239bd4d08dd3c621de33960e1a65f589cec2
AnjaliMohan1 <anju.anjalimohan@gmail.com>
2025-12-09 17:58:07 +0530

    Update streamlit_app.py
    New section to display smoothiefroot information

--- streamlit_app.py
+++ streamlit_app.py
@@ -56,7 +56,8 @@ if ingredients_list:
     
       st.success('Your Smoothie is ordered,'+name_on_order+'!', icon="✅")
 
-#New section to display smoothiefruit information
+#New section to display smoothiefroot information
 import requests
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response.json())
+#st.text(smoothiefroot_response.json())
+sf_df=st.dataframe(data=smoothiefroot.response.json(),use_container_width=True)
